f0fdc2f1d92c19179396aaa713e65954b8855aa9
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-26 17:19:11 +0300

    orm

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,57 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)  # <Department 2: HR, Building F, 10th Floor>
+
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+ipdb.set_trace()
+
+#!/usr/bin/env python3
+#lib/testing/debug.py
+
+from __init__ import CONN, CURSOR
+from department import Department
+
+import ipdb
+
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create('payroll', "Building A, 5th Floor")
+print(payroll)
+
+
+sales = Department.create('sales and marketing', "Building B, 4th Floor")
+print(sales)
+
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+print("Delete payroll")
+payroll.delete()
+print(payroll)
+
+
+
 ipdb.set_trace()
